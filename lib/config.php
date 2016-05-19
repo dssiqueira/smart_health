541d9e1fd2409ae340071f0c0d81da194910392c
Douglas Siqueira <douglas.siqueira.silva@gmail.com>
2016-05-19 18:41:22 -0300

    Config

---
+++ lib/config.php
@@ -0,0 +1,19 @@
+<?php
+class mysqlConnection { 
+    public $hots        = 'xxxxxxxxxx'; 
+    public $database    = 'xxxxxxxxxx';
+    public $user        = 'xxxxxxxxxx';
+    public $pass        = 'xxxxxxxxxx';   
+} 
+
+class apiStrava {
+    public $client_id       = 'xxxxxxxxxx';
+    public $client_secret   = 'xxxxxxxxxx';
+}
+
+class apiRuneeper {
+    public $client_id       = 'xxxxxxxxxx';
+    public $client_secret   = 'xxxxxxxxxx';
+}
+
+?>
