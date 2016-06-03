323b5e33d469330d5a86fff1d88c2682e5d2630b
Douglas Siqueira <Douglas Siqueira>
2016-06-03 00:44:04 -0300

    Redirect For Dashboard

--- saveIntegration.php
+++ saveIntegration.php
@@ -1,9 +1,6 @@
 <?php
-error_reporting(E_ERROR);
-
 require('lib/runkeeperAPI.class.php');
 require('lib/config.php');
-
 
 /* API initialization */
 $rkAPI = new runkeeperAPI();
@@ -12,17 +9,15 @@ $rkAPI = new runkeeperAPI();
 if ($_GET['code']) {
 	$auth_code = $_GET['code'];
 
-
 	if ($rkAPI->getRunkeeperToken($auth_code) == false) {
 		echo $rkAPI->api_last_error; /* get access token problem */
 		exit();
 	}
 	else {
 		$insert = new mysqlConnection;
-        $query  = 'INSERT INTO INTEGRATION (appid,uid,token) VALUE (1,5, "' . $rkAPI->access_token . '")';
-		var_dump($query);
+        $query  = 'INSERT INTO integration(appid,uid,token) VALUE (1,1, "' . $rkAPI->access_token . '")';
         $insert->mysqlQuery($query);
-		echo "ok o token é >>>>> " . $rkAPI->access_token;
+		header("location:app.php");
 	}
 }
 ?>
