67a834f00ac37072367cb15eac0d5f7a51a1c2e9
Douglas Siqueira <Douglas Siqueira>
2016-06-07 21:25:08 -0300

    Alter Home Page Layout

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   <head>
     <script src="https://apis.google.com/js/platform.js" async defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
     <meta name="google-signin-client_id" content="1004959689078-0tc7p0enbjr3eq9h2p2j72pmt1g0g7u2.apps.googleusercontent.com">
     <script>
       function post(path, params, method) {
@@ -26,20 +27,28 @@
     </script>
     <style>
       html { 
-        background: url(images/error.gif) no-repeat center center fixed; 
+        font-family: 'Roboto', sans-serif;
+        background: url(images/health.jpg) no-repeat center center fixed; 
         -webkit-background-size: cover;
         -moz-background-size: cover;
         -o-background-size: cover;
         background-size: cover;
+        text-align: center;
+      }
+      h1 {
+        margin-top: 0px;
+        color: #fff;
+        text-decoration: none;
+        font-size: 60px;
       }
     </style>
   </head>
   <body class="homepage">
-    <div id="logo">
+    <div id="logo" style="margin-top: 160px;">
       <img src="images/CIT.png" width="30%"></img>
-      <h1><a href="#">Health</a></h1>
-      <div class="g-signin2" data-onsuccess="onSignIn"></div>
+      <h1>Health</h1>
     </div>
+    <div id="g-signin2" style="position: absolute; right: 50%; margin-right: -120px;"></div>
     <script>
       function onSignIn(googleUser) {
         var profile = googleUser.getBasicProfile();
@@ -50,6 +59,15 @@
           window.location = "error.php";
         }
       }
+      function renderButton() {
+        gapi.signin2.render('g-signin2', {
+          'scope': 'profile email',
+          'width': 240,
+          'height': 50,
+          'longtitle': true
+        });
+      }
     </script>
+    <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 	</body>
 </html>
