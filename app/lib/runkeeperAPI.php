a48eaa665e70c052e91dd6024afa4d91117bc91a
Henrique Guidi <henrique.guidi@gmail.com>
2016-10-23 21:06:34 -0200

    just a test, I know bad idea to do that here, but I don't have environment to test =(

--- app/lib/runkeeperAPI.php
+++ app/lib/runkeeperAPI.php
@@ -268,4 +268,34 @@ class runkeeperAPI {
 		
 		return json_decode ( $response, true );
 	}
+
+	/*
+	 * 
+	 */
+	public function getAllActivities() {
+		$curl = curl_init ();
+		
+		curl_setopt_array ( $curl, array (
+				CURLOPT_URL => "http://api.runkeeper.com/fitnessActivities?pageSize=99",
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_HTTPHEADER => array (
+						"accept: application/vnd.com.runkeeper.FitnessActivityFeed+json",
+						"authorization: Bearer " . $this->access_token,
+						"cache-control: no-cache" 
+				) 
+		) );
+		
+		$response = curl_exec ( $curl );
+		$err = curl_error ( $curl );
+		
+		curl_close ( $curl );
+		
+		return json_decode ( $response, true );
+	}
+
 }
